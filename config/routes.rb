629c172541fd5d574eb2db438c0ebe350d4afdf2
NamHyeongKeol <grey_pastor@naver.com>
2017-09-09 05:47:22 +0900

    friend, friend_admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get '/event' => 'event#index'
   get '/friend' => 'friend#index'
-  get '/friend_admin' => 'friend#friend_admin'
+  get '/friend_admin' => 'friend#index_admin'
   get '/group' => 'group#index'
 
   get '/event/show/:id' => 'event#show'
@@ -13,8 +13,12 @@ Rails.application.routes.draw do
 
   post '/event/create' => 'event#create'
   post '/friend/create' => 'friend#create'
+  post '/friend/create_admin' => 'friend#create_admin'
   post '/group/create' => 'group#create'
 
   post '/friend/post' => 'friend#post'
+  post '/friend/post_admin' => 'friend#post_admin'
+
   delete '/friend/destroy' => 'friend#destroy'
+  delete '/friend/destroy_admin' => 'friend#destroy_admin'
 end
