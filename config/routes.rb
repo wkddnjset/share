8a1c831671fdfa9c6d42fd962da7f10f4c63ae1b
NamHyeongKeol <grey_pastor@naver.com>
2017-09-09 00:27:48 +0900

    User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'event#index'
-  
+
   get '/event' => 'event#index'
   get '/friend' => 'friend#index'
   get '/group' => 'group#index'
-  
+
   get '/event/show' => 'event#show'
   get '/friend/show' => 'friendt#show'
   get '/group/show' => 'group#show'
-  
+
   post '/event/creat' => 'event#create'
   post '/friend/create' => 'friend#create'
   post '/group/create' => 'group#create'
-  
+
 end
